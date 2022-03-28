19fc4536e483d28551ac1d668e1e25c19ae4cfc9
nadahkhaledd <nadahkhaledd@gmail.com>
2022-03-28 20:26:22 +0200

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import json
+
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout,
+                             QPushButton, QLabel, QLineEdit, QTabBar, QFrame, QStackedLayout)
+
+
