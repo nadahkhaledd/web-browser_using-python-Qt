1a592fe0cb9b8ee0fc2e892b1f8714850d56ab8e
Nadah Khaled <nadahkhaledd@gmail.com>
2022-04-14 22:07:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import os 
 import json
 
-# from PyQt5.QtWidgets import * 
+
 
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout,
                              QPushButton, QLabel, QLineEdit, QTabBar, QFrame, QStackedLayout)
