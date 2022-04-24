cb3ca6aa44869aa7bd29ce3a46e7053aa6e36e87
nadahkhaledd <nadahkhaledd@gmail.com>
2022-04-25 00:59:32 +0200

    done imports

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ import sys
 import os 
 import json
 
-
-
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout,
                              QPushButton, QLabel, QLineEdit, QTabBar, QFrame, QStackedLayout)
+from PyQt5.QtGui import QIcon, QImage, QWindow
+from PyQt5.QtCore import *
+
+class App(QFrame):
+    def __init__(self):
+        self.setWindowTitle("web browser")
+        self.setBaseSize(1366, 768)
+        self.createApp()
+
+        def createApp(self):
+            self.layout = QVBoxLayout()
+            self.tabBar = QTabBar()
+            self.tabBar.addTab("tab1")
+            self.tabBar.addTab("tab2")
+
 
 
